bb0c5fb8bb6aad1252cc1e8cbb66e04e3f3e9e46
unknown <dev@DSN1191348068.edu119.local>
2026-02-09 11:11:52 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     public static void menuPrincipal(){
-        System.out.println("=== Secretaria ===");
+        System.out.println("\n=== Secretaria ===");
         System.out.println("1 - Alunos");
         System.out.println("2 - Turmas");
         System.out.println("Sair");
@@ -28,12 +28,13 @@ public class Main {
                 break;
             default:
                 System.out.println("Opção inválida! Tente novamente");
+                menuPrincipal();
         }
 
     }
 
     private static void menuTurmas() {
-        System.out.println("=== Turmas ===");
+        System.out.println("\n=== Turmas ===");
         System.out.println("1 - Listar Turmas");
         System.out.println("2 - Cadastrar Turmas");
         System.out.println("3 - Atualizar Turma");
@@ -43,26 +44,31 @@ public class Main {
         switch (opcao) {
             case "1":
                 listarTurmas();
+                menuTurmas();
                 break;
             case "2":
                 cadastrarTurma();
+                menuTurmas();
                 break;
             case "3":
                 atualizarTurma();
+                menuTurmas();
                 break;
             case "4":
                 excluirTurma();
+                menuTurmas();
                 break;
             case "5":
                 menuPrincipal();
                 break;
             default:
                 System.out.println("Opção Inválida! Tente novamente");
+                menuTurmas();
         }
     }
 
     private static void menuAlunos() {
-        System.out.println("=== Alunos ===");
+        System.out.println("\n=== Alunos ===");
         System.out.println("1 - Listar Alunos");
         System.out.println("2 - Cadastrar Aluno");
         System.out.println("3 - Atualizar Aluno");
@@ -72,20 +78,25 @@ public class Main {
         switch (opcao) {
             case "1":
                 listarAlunos();
+                menuAlunos();
                 break;
             case "2":
                 cadastrarAluno();
+                menuAlunos();
                 break;
             case "3":
                 atualizarAluno();
+                menuAlunos();
                 break;
             case "4":
                 excluirAluno();
+                menuAlunos();
                 break;
             case "5":
                 break;
             default:
                 System.out.println("Opção Inválida! Tente novamente");
+                menuAlunos();
         }
     }
 
@@ -93,14 +104,72 @@ public class Main {
     }
 
     private static void atualizarTurma() {
-        
     }
 
     private static void cadastrarTurma() {
-        
+        Periodo periodo = validarPeriodo();
+
+        String curso = Leitura.dados("Digite o curso:");
+        while(!isCharacter(curso)) {
+            System.out.println("Nome de Curso inválido! Não use números ou caracteres especiais, por favor");
+            curso = Leitura.dados("Digite o curso:");
+        }
+
+        String sigla = Leitura.dados("Digite a sigla:");
+        boolean repetido = true;
+        while(sigla.isBlank() || !repetido) {
+            System.out.println("Sigla inválida!");
+            sigla = Leitura.dados("Digite a sigla:");
+            sigla = sigla.toUpperCase();
+
+            for(Turma t : listaTurmas){
+                if(t.getSigla().equals(sigla)){
+                    System.out.println("Turma já cadastrada!");
+                    repetido = true;
+        }
+            }
+            repetido = false;
+        }
+
+
+        Turma turma = new Turma(curso, sigla, periodo);
+
+        listaTurmas.add(turma);
+    }
+
+    private  static boolean isCharacter(String texto){
+        String textoSemNumeros = texto.replaceAll("\\d","");
+        return !texto.isBlank() && texto.equals(textoSemNumeros);
+    }
+
+
+    private static Periodo validarPeriodo(){
+        String opcaoPeriodo = Leitura.dados("""
+                Digite o número do período escolhido:
+                1 - Matutino
+                2 - Vespertino
+                3 - Norutno
+                4 - Integral""");
+        switch (opcaoPeriodo){
+            case"1":
+                return  Periodo.MATUTINO;
+            case"2":
+                return  Periodo.VERSPERTINO;
+            case"3":
+                return  Periodo.NORTUNO;
+            case"4":
+                return  Periodo.INTEGRAL;
+            default:
+                System.out.println("Opção inválida, digite novamente:");
+                return validarPeriodo();
+        }
     }
 
     private static void listarTurmas() {
+        if(listaTurmas.isEmpty()) {
+            System.out.println("Não há turmas cadastradas");
+            return;
+        }
         for(Turma t: listaTurmas){
             System.out.println(t);
         }
@@ -117,6 +186,10 @@ public class Main {
     }
 
     private static void listarAlunos() {
+        if(listaAlunos.isEmpty()) {
+            System.out.println("Não há Alunos cadastrados");
+            return;
+        }
         for(Aluno a: listaAlunos){
             System.out.println(a);
         }
--- src/Turma.java
+++ src/Turma.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+
 public class Turma {
     private String curso;
     private String sigla;
     private Periodo periodo;
+
+    public Turma(String curso, String sigla, Periodo periodo) {
+        this.curso = "";
+        this.sigla = "";
+        this.periodo = "";
+    }
+
+    public Turma(String curso, String sigla, Periodo periodo) {
+        this.curso = curso;
+        this.sigla = sigla;
+        this.periodo = periodo;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Periodo getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Periodo periodo) {
+        this.periodo = periodo;
+    }
 }
